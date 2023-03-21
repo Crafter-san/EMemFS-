9850657760e667e6cfffba325f7bb1e4c04ac725
Crafter-san <74330895+Crafter-san@users.noreply.github.com>
2023-03-21 12:42:41 -0700

    organized a bit

--- MainProgram.cs
+++ MainProgram.cs
@@ -7,16 +7,18 @@ public class MainProgram {
         Device device = new("test device");
         Folder folder = new("test");
         Task.Delay(2000).GetAwaiter().GetResult();
-        File file =  new ("test");
+        File file = new("test");
+        device.Folders.Set(folder);
         folder.Files.Set(file);
         folder.Files.Get("test").Append("test");
         file.Append("tester", true);
-        device.Folders.Set(folder);
         Console.WriteLine(folder.Files.Get("test").Timestamp);
         Console.WriteLine(folder.Timestamp);
         DeviceManager.Save("test file", device);
         Device device_instance = DeviceManager.Load("test file");
-        
+
         Console.WriteLine(device_instance.Folders.Get("test").Files.Get("test").Lines[0]);
+        Console.WriteLine(device_instance.Folders.Get("test").Files.Get("test").Timestamp);
+        Console.WriteLine(device_instance.Folders.Get("test").Timestamp);
     }
 }
