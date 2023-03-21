96c56513c6b157d6b19577d39593ba1f0d8d0039
Crafter-san <74330895+Crafter-san@users.noreply.github.com>
2023-03-21 08:38:00 -0700

    Add files via upload

---
+++ MainProgram.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using System;
+//using System.ComponentModel;
+
+
+using System.Numerics;
+using System.Threading;
+
+public class MainProgram {
+    public static void Main() {
+        Folder folder = new("test");
+        Thread.Sleep(2000);
+        File file =  new ("test");
+        folder.Files.Set(file);
+        folder.Files.Get("test").Append("test");
+        file.Append("tester", true);
+        Console.WriteLine(folder.Files.Get("test").Timestamp);
+        Console.WriteLine(folder.Timestamp);
+    }
+}
