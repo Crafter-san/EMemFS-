78a7c5459050e5a72b355eeba8737bfb015428a0
Crafter-san <74330895+Crafter-san@users.noreply.github.com>
2023-03-21 17:10:55 -0700

    Update MainProgram.cs

--- MainProgram.cs
+++ MainProgram.cs
@@ -4,18 +4,18 @@ using System.Text.Json;
 
 public class MainProgram {
     public static void Main() {
-        Device device = new("test device");
-        Folder folder = new("test");
+        EMemFS.Device device = new("test device");
+        EMemFS.Folder folder = new("test");
         Task.Delay(2000).GetAwaiter().GetResult();
-        File file = new("test");
+        EMemFS.File file = new("test");
         device.Folders.Set(folder);
         folder.Files.Set(file);
         folder.Files.Get("test").Append("test");
         file.Append("tester", true);
         Console.WriteLine(folder.Files.Get("test").Timestamp);
         Console.WriteLine(folder.Timestamp);
-        DeviceManager.Save("test file", device);
-        Device device_instance = DeviceManager.Load("test file");
+        EMemFS.DeviceManager.Save("test file", device);
+        EMemFS.Device device_instance = EMemFS.DeviceManager.Load("test file");
 
         Console.WriteLine(device_instance.Folders.Get("test").Files.Get("test").Lines[0]);
         Console.WriteLine(device_instance.Folders.Get("test").Files.Get("test").Timestamp);
