203a3b1d21940f9f1c55cc1be29a0dd7969bdf9d
Crafter-san <74330895+Crafter-san@users.noreply.github.com>
2023-03-21 17:10:09 -0700

    Update EMemFS.cs

--- EMemFS.cs
+++ EMemFS.cs
@@ -6,142 +6,145 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-public abstract class EFSData
+public class EMemFS
 {
-    public string name;
-    public string data;
-    public Dictionary<string, string> metadata;
-    public DateTime Timestamp { get; init; }
-    public EFSData()
+    public abstract class EFSData
     {
-        Timestamp = DateTime.Now;
-    }
-}
-public class Manage<EFSObj> where EFSObj : EFSData
-{
-    public List<string> Dir { get; init; } = new();
-    public Dictionary<string, EFSObj> List { get; init; } = new();
-    public EFSObj Get(string key)
-    {
-        if (!Dir.Contains(key)) return null;
-        return List[key];
-    }
-    public EFSObj Set(EFSObj obj)
-    {
-        if (!Dir.Contains(obj.name)) Dir.Add(obj.name);
-        List[obj.name] = obj;
-        return obj;
-    }
-    public EFSObj Destroy(EFSObj obj)
-    {
-        List.Remove(obj.name);
-        Dir.Remove(obj.name);
-        return obj;
-    }
-}
-public class File : EFSData
-{
-    public List<string> Lines { get; init; } = new() { "" };
-    public File(string name = "Default Filename", string data = "")
-    {
-
-        this.name = name;
-        this.data = data;
-    }
-    public string Append(string line, bool newline = false)
-    {
-        if (newline)
+        public string name;
+        public string data;
+        public Dictionary<string, string> metadata;
+        public DateTime Timestamp { get; init; }
+        public EFSData()
         {
-            Lines.Add(line);
-            line = $"\n{line}";
+            Timestamp = DateTime.Now;
         }
-        else Lines[^1] += line;
-        data += line;
-        return Lines[^1];
     }
-}
-public class Folder : EFSData
-{
-    public Manage<File> Files { get; init; }
-    public Manage<Folder> Folders { get; init; }
-    public Folder(string name, Manage<File> files = null, Manage<Folder> folders = null)
+    public class Manage<EFSObj> where EFSObj : EFSData
     {
-        Files = files ?? new();
-        Folders = folders ?? new();
-        this.name = name;
+        public List<string> Dir { get; init; } = new();
+        public Dictionary<string, EFSObj> List { get; init; } = new();
+        public EFSObj Get(string key)
+        {
+            if (!Dir.Contains(key)) return null;
+            return List[key];
+        }
+        public EFSObj Set(EFSObj obj)
+        {
+            if (!Dir.Contains(obj.name)) Dir.Add(obj.name);
+            List[obj.name] = obj;
+            return obj;
+        }
+        public EFSObj Destroy(EFSObj obj)
+        {
+            List.Remove(obj.name);
+            Dir.Remove(obj.name);
+            return obj;
+        }
     }
-}
-public class User : EFSData
-{
-    public Manage<File> Files { get; init; }
-    public Manage<Folder> Folders { get; init; }
-    public Manage<User> Users { get; init; }
-    public User(string name, Manage<File> files = null, Manage<Folder> folders = null, Manage<User> users = null)
+    public class File : EFSData
     {
-        Files = files ?? new();
-        Folders = folders ?? new();
-        Users = users ?? new();
-        this.name = name;
+        public List<string> Lines { get; init; } = new() { "" };
+        public File(string name = "Default Filename", string data = "")
+        {
+
+            this.name = name;
+            this.data = data;
+        }
+        public string Append(string line, bool newline = false)
+        {
+            if (newline)
+            {
+                Lines.Add(line);
+                line = $"\n{line}";
+            }
+            else Lines[^1] += line;
+            data += line;
+            return Lines[^1];
+        }
+    }
+    public class Folder : EFSData
+    {
+        public Manage<File> Files { get; init; }
+        public Manage<Folder> Folders { get; init; }
+        public Folder(string name, Manage<File> files = null, Manage<Folder> folders = null)
+        {
+            Files = files ?? new();
+            Folders = folders ?? new();
+            this.name = name;
+        }
+    }
+    public class User : EFSData
+    {
+        public Manage<File> Files { get; init; }
+        public Manage<Folder> Folders { get; init; }
+        public Manage<User> Users { get; init; }
+        public User(string name, Manage<File> files = null, Manage<Folder> folders = null, Manage<User> users = null)
+        {
+            Files = files ?? new();
+            Folders = folders ?? new();
+            Users = users ?? new();
+            this.name = name;
+        }
+
+    }
+    public class Drive : EFSData
+    {
+        public Manage<File> Files { get; init; }
+        public Manage<Folder> Folders { get; init; }
+        public Manage<User> Users { get; init; }
+        public Manage<Drive> Drives { get; init; }
+        public Drive(string name, Manage<File> files = null, Manage<Folder> folders = null, Manage<User> users = null, Manage<Drive> drives = null)
+        {
+            Files = files ?? new();
+            Folders = folders ?? new();
+            Users = users ?? new();
+            Drives = drives ?? new();
+            this.name = name;
+
+        }
     }
 
-}
-public class Drive : EFSData
-{
-    public Manage<File> Files { get; init; }
-    public Manage<Folder> Folders { get; init; }
-    public Manage<User> Users { get; init; }
-    public Manage<Drive> Drives { get; init; }
-    public Drive(string name, Manage<File> files = null, Manage<Folder> folders = null, Manage<User> users = null, Manage<Drive> drives = null)
+    public class Device : EFSData
     {
-        Files = files ?? new();
-        Folders = folders ?? new();
-        Users = users ?? new();
-        Drives = drives ?? new();
-        this.name = name;
-
+        public Manage<File> Files { get; init; }
+        public Manage<Folder> Folders { get; init; }
+        public Manage<User> Users { get; init; }
+        public Manage<Drive> Drives { get; init; }
+        public Manage<Device> Devices { get; init; }
+        public Device(string name, Manage<File> files = null, Manage<Folder> folders = null, Manage<User> users = null, Manage<Drive> drives = null, Manage<Device> devices = null)
+        {
+            Files = files ?? new();
+            Folders = folders ?? new();
+            Users = users ?? new();
+            Drives = drives ?? new();
+            Devices = devices ?? new();
+            this.name = name;
+        }
     }
-}
 
-public class Device : EFSData
-{
-    public Manage<File> Files { get; init; }
-    public Manage<Folder> Folders { get; init; }
-    public Manage<User> Users { get; init; }
-    public Manage<Drive> Drives { get; init; }
-    public Manage<Device> Devices { get; init; }
-    public Device(string name, Manage<File> files = null, Manage<Folder> folders = null, Manage<User> users = null, Manage<Drive> drives = null, Manage<Device> devices = null)
+    public class DeviceManager
     {
-        Files = files ?? new();
-        Folders = folders ?? new();
-        Users = users ?? new();
-        Drives = drives ?? new();
-        Devices = devices ?? new();
-        this.name = name;
-    }
-}
+        public static JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            IncludeFields = true,
+            WriteIndented = true
+        };
+        public static void Save(string filename, Device device)
+        {
 
-public class DeviceManager
-{
-    public static JsonSerializerOptions options = new JsonSerializerOptions
-    {
-        IncludeFields = true,
-        WriteIndented = true
-    };
-    public static void Save(string filename, Device device)
-    {
+            string jsonString = JsonSerializer.Serialize<Device>(device, options);
+            using var streamWriter = System.IO.File.CreateText(filename);
+            streamWriter.Write(jsonString);
+            streamWriter.Close();
+        }
+        public static Device Load(string filename)
+        {
 
-        string jsonString = JsonSerializer.Serialize<Device>(device, options);
-        using var streamWriter = System.IO.File.CreateText(filename);
-        streamWriter.Write(jsonString);
-        streamWriter.Close();
-    }
-    public static Device Load(string filename)
-    {
-
-        using StreamReader streamReader = new(filename);
-        Device? device_instance =
-                JsonSerializer.Deserialize<Device>(streamReader.ReadToEnd(), options);
-        streamReader.Close();
-        return device_instance;
+            using StreamReader streamReader = new(filename);
+            Device? device_instance =
+                    JsonSerializer.Deserialize<Device>(streamReader.ReadToEnd(), options);
+            streamReader.Close();
+            return device_instance;
+        }
     }
 }
